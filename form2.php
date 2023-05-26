dda75c26344ada0ee7f2e29e50059299ce55114c
ariffmhdd <ariffmhdd>
2023-05-27 02:03:33 +0700

    last

--- form2.php
+++ form2.php
@@ -9,7 +9,8 @@ require_once 'functions.php';
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Form Add</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
 </head>
 
@@ -17,7 +18,8 @@ require_once 'functions.php';
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="#home">UKM UPNVJT</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
@@ -45,16 +47,20 @@ require_once 'functions.php';
                         </div>
                         <div class="mb-3">
                             <label for="sp" class="form-label">Logo UKM</label>
-                            <input class="form-control" type="file" id="img" name="img" accept=".img, .jpeg, .png ,.jpg"/>
+                            <input class="form-control" type="file" id="img" name="img"
+                                accept=".img, .jpeg, .png ,.jpg" />
                         </div>
                     </td>
                 </tr>
             </table>
-            <a href="index.php" class="btn btn-secondary">CANCEL</a>
-            <button class="btn btn-primary" type="submit" value="ukmADD" name="proses">SUBMIT</button>
+            <a href="index.php" class="btn btn-secondary">Batal</a>
+            <button class="btn" style="color: white;
+  background-color: #617a55;" type="submit" value="ukmADD" name="proses">Simpan</button>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
