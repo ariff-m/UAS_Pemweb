7c16c71fa8f59f6fe5b65d49c2cdff2c597f6467
ariffmhdd <ariffmhdd>
2023-05-26 23:11:37 +0700

    new

--- guest.php
+++ guest.php
@@ -13,7 +13,7 @@ $result = mysqli_query($conn, $query);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UKM UPNVJT - Home</title>
+    <title>UKM UPNVJT - User</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="guest.css" />
