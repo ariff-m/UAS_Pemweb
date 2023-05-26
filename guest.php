8e1e37c09eda426bdcc466659567246a34f3bc35
ariffmhdd <ariffmhdd>
2023-05-26 22:20:11 +0700

    new

--- guest.php
+++ guest.php
@@ -123,7 +123,10 @@ $result = mysqli_query($conn, $query);
     </div>
 
     <footer>
-        <p>&copy 2023 | Kelompok 4</p>
+        <div class="container">
+            <p>Jalan Raya Rungkut Madya<br>Surabaya, 60295<br>Indonesia</p>
+            <p class="copy">Copyright &copy 2023 | All Rights Reserved | Kelompok 4</p>
+        </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
