bc5ada71456e1e1154a966257391361ad6ea6eeb
ariffmhdd <ariffmhdd>
2023-05-27 00:31:51 +0700

    new

--- data.php
+++ data.php
@@ -25,7 +25,8 @@ $result = mysqli_query($conn, $query);
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
     <link rel="stylesheet" href="data.css" />
 </head>
 
@@ -33,15 +34,17 @@ $result = mysqli_query($conn, $query);
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php">UKM UPNVJT</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                    <a class="nav-link" href="data.php">Data</a>
+                    <a class="nav-link" aria-current="page" href="index.php">Data UKM</a>
+                    <a class="nav-link" href="data.php">Data Mahasiswa</a>
                     <div class="dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Form</a>
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">Formulir</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="form.php">Tambah Mahasiswa</a></li>
                             <li><a class="dropdown-item" href="form2.php">Tambah UKM</a></li>
@@ -77,21 +80,34 @@ $result = mysqli_query($conn, $query);
                 if (mysqli_num_rows($result) > 0) {
                     $no = 1;
                     while ($row = mysqli_fetch_assoc($result)) {
-                ?>
+                        ?>
                         <tr>
-                            <td><?php echo $no++; ?></td>
-                            <td><?php echo $row['npm']; ?></td>
-                            <td><?php echo $row['nama']; ?></td>
-                            <td><?php echo $row['jur']; ?></td>
-                            <td><?php echo $row['jk']; ?></td>
-                            <td><?php echo $row['ukm']; ?></td>
+                            <td>
+                                <?php echo $no++; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['npm']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['nama']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['jur']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['jk']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['ukm']; ?>
+                            </td>
                             <td>
                                 <a type="button" class="btn btn-warning" href="detail.php?id=<?= $row['id'] ?>">DETAIL</a> |
                                 <a type="button" class="btn btn-success" href="edit.php?id=<?= $row['id'] ?>">EDIT</a> |
-                                <a type="button" class="btn btn-danger" href="functions.php?id=<?= $row['id'] ?> &proses=remove">REMOVE</a>
+                                <a type="button" class="btn btn-danger"
+                                    href="functions.php?id=<?= $row['id'] ?> &proses=remove">REMOVE</a>
                             </td>
                         </tr>
-                <?php
+                        <?php
                     }
                 }
                 ?>
@@ -108,10 +124,12 @@ $result = mysqli_query($conn, $query);
     <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('#data-table').DataTable();
         });
     </script>
--- index.php
+++ index.php
@@ -25,7 +25,8 @@ $result = mysqli_query($conn, $query);
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css" />
 </head>
 
@@ -33,7 +34,8 @@ $result = mysqli_query($conn, $query);
   <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand" href="index.php">UKM UPNVJT</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
@@ -41,7 +43,8 @@ $result = mysqli_query($conn, $query);
           <a class="nav-link" aria-current="page" href="index.php">Data UKM</a>
           <a class="nav-link" href="data.php">Data Mahasiswa</a>
           <div class="dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Fomulir</a>
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">Formulir</a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="form.php">Tambah Mahasiswa</a></li>
               <li><a class="dropdown-item" href="form2.php">Tambah UKM</a></li>
@@ -64,23 +67,31 @@ $result = mysqli_query($conn, $query);
       if (mysqli_num_rows($result) > 0) {
         $no = 1;
         while ($row = mysqli_fetch_assoc($result)) {
-      ?>
+          ?>
           <div class="card" style="width: 15rem;">
             <img src="uploads/<?php echo $row['img']; ?>" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5><?php echo $row['ukm']; ?></h5>
-              <p class="card-text"><?php echo $row['desk'] ?></p>
-              <p class="fw-semibold">Total Mahasiswa : <?php echo $row['total'] ?></p>
-              <a href="<?php echo $row['instagram'] ?>" class="sosmed text-decoration-none btn" target="_blank"><i class="bi bi-instagram"></i> Instagram</a>
+              <h5>
+                <?php echo $row['ukm']; ?>
+              </h5>
+              <p class="card-text">
+                <?php echo $row['desk'] ?>
+              </p>
+              <p class="fw-semibold">Total Mahasiswa :
+                <?php echo $row['total'] ?>
+              </p>
+              <a href="<?php echo $row['instagram'] ?>" class="sosmed text-decoration-none btn" target="_blank"><i
+                  class="bi bi-instagram"></i> Instagram</a>
               <hr>
               <div>
                 <a type="button" class="btn btn-warning" href="edit2.php?id=<?= $row['id'] ?>">EDIT</a>
-                <a type="button" class="btn btn-danger" href="functions.php?id=<?= $row['id'] ?> &proses=ukmREMOVE">REMOVE</a>
+                <a type="button" class="btn btn-danger"
+                  href="functions.php?id=<?= $row['id'] ?> &proses=ukmREMOVE">REMOVE</a>
               </div>
             </div>
           </div>
 
-      <?php
+          <?php
         }
       }
       ?>
@@ -96,10 +107,12 @@ $result = mysqli_query($conn, $query);
   <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
 
   <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
       $('#data-table').DataTable();
     });
   </script>
