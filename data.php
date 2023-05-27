b23b6b5263eb621c2a42c1a3367b1bc0e756fb38
ariffmhdd <ariffmhdd>
2023-05-27 13:05:48 +0700

    last

--- data.php
+++ data.php
@@ -20,12 +20,13 @@ $result = mysqli_query($conn, $query);
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>UKM</title>
+    <title>UKM UPNVJT - Data Mahasiswa</title>
     <!-- Data Tables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
     <link rel="stylesheet" href="data.css" />
 </head>
 
@@ -33,7 +34,8 @@ $result = mysqli_query($conn, $query);
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php">UKM UPNVJT</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
@@ -41,7 +43,8 @@ $result = mysqli_query($conn, $query);
                     <a class="nav-link" aria-current="page" href="index.php">Data UKM</a>
                     <a class="nav-link" href="data.php">Data Mahasiswa</a>
                     <div class="dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Formulir</a>
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">Formulir</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="form.php">Tambah Mahasiswa</a></li>
                             <li><a class="dropdown-item" href="form2.php">Tambah UKM</a></li>
@@ -78,7 +81,7 @@ $result = mysqli_query($conn, $query);
                     if (mysqli_num_rows($result) > 0) {
                         $no = 1;
                         while ($row = mysqli_fetch_assoc($result)) {
-                    ?>
+                            ?>
                             <tr>
                                 <td>
                                     <?php echo $no++; ?>
@@ -101,10 +104,12 @@ $result = mysqli_query($conn, $query);
                                 <td style="width: 25%;">
                                     <a type="button" class="btn btn-dark" href="detail.php?id=<?= $row['id'] ?>">DETAIL</a> |
                                     <a type="button" class="btn btn-warning" href="edit.php?id=<?= $row['id'] ?>">EDIT</a> |
-                                    <a type="button" class="btn btn-danger" href="functions.php?id=<?= $row['id'] ?> &proses=remove" onclick="return confirm('Apakah kamu yakin menghapus data NPM : <?= $row['npm']; ?> ?');">REMOVE</a>
+                                    <a type="button" class="btn btn-danger"
+                                        href="functions.php?id=<?= $row['id'] ?> &proses=remove"
+                                        onclick="return confirm('Apakah kamu yakin menghapus data NPM : <?= $row['npm']; ?> ?');">REMOVE</a>
                                 </td>
                             </tr>
-                    <?php
+                            <?php
                         }
                     }
                     ?>
@@ -125,10 +130,12 @@ $result = mysqli_query($conn, $query);
     <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('#data-table').DataTable();
         });
     </script>
--- login.php
+++ login.php
@@ -36,9 +36,10 @@ if (isset($_POST['login'])) {
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Login UKM</title>
+  <title>UKM UPNVJT - Login</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
   <link rel="stylesheet" href="login.css" />
   <style>
     .form-group {
@@ -61,7 +62,8 @@ if (isset($_POST['login'])) {
   <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand" href="guest.php">UKM UPNVJT</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
@@ -94,14 +96,16 @@ if (isset($_POST['login'])) {
         </div>
       </div>
       <div style="display: none;" class="verif text-danger fw-bold text-center <?php if (isset($error))
-                                                                                  echo 'd-block'; ?>">
+        echo 'd-block'; ?>">
         <p>Username or password is incorrect!</p>
       </div>
       <button class="btn btn-secondary btnlogin" type="submit" name="login">LOGIN</button>
     </form>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
 
   <script>
     function togglePassword() {
