8933365c1549b5859e74577a24c920ef446ba60f
VolemAzira <volemalvaroazira2@gmail.com>
2023-05-28 14:44:08 +0700

    new

--- detail.php
+++ detail.php
@@ -35,7 +35,7 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Form Edit</title>
+    <title>Details</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
 </head>
